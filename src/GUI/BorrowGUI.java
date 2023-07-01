892f361239afa52c3269919d6e0c30211bf4bb09
dingquanhong <62588101+dingquanhong@users.noreply.github.com>
2023-07-01 09:38:40 +0800

    updata

--- src/GUI/BorrowGUI.java
+++ src/GUI/BorrowGUI.java
@@ -3,19 +3,28 @@ package GUI;
 import fun.Window;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class BorrowGUI {
     private JPanel borrow;
     private JComboBox comboBox1;
     private JTextField textField1;
     private JButton 搜索Button;
-    private JButton 借阅Button;
-    private JButton 加入书单Button;
+    private JButton B1;
+    private JButton A1;
     private JButton button1;
     private JButton button2;
     private JButton button3;
     private JButton button4;
+    public BorrowGUI(){
+        B1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
+            }
+        });
+    }
     public static void show() {
         JFrame frame = new JFrame("BorrowGUI");
         frame.setContentPane(new BorrowGUI().borrow);
---
+++ src/fun/BorrowFun.java
@@ -0,0 +1,8 @@
+package fun;
+import fun.*;
+public class BorrowFun {
+    public static boolean BorrowbyID(int bookid){
+
+        return false;
+    }
+}
